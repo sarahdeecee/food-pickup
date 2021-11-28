9915eb52293d67ca87451e1e23b1cae3049384c2
Miguel <mige.d@outlook.com>
2021-11-28 16:17:41 -0500

    add insert query for order_foods

--- routes/orders.js
+++ routes/orders.js
@@ -9,40 +9,37 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  // router.get("/", (req, res) => {
-  //   let query = `SELECT * FROM widgets`;
-  //   console.log(query);
-  //   db.query(query)
-  //     .then((data) => {
-  //       const widgets = data.rows;
-  //       res.json({ widgets });
-  //     })
-  //     .catch((err) => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
   router.post("/", (req, res) => {
-    const ordersQuery = `
+    const query = `
       INSERT INTO orders (customer_id, subtotal, tax)
       VALUES ($1, $2, $3)
       RETURNING *;
     `;
     const values = [req.body.user_id, req.body.subtotal, req.body.tax];
-    console.log(req.body.items);
 
-    db.query(ordersQuery, values)
-      .then((res) => console.log(res.rows))
+    db.query(query, values)
+      .then((res) => {
+        console.log(res.rows[0]);
+        insertOrderFoods(res.rows[0].id, req.body.items);
+      })
       .catch((err) => {
         console.log('query error:', err.message);
       });
+  });
 
-    const orderfoodsQuery = `
+  const insertOrderFoods = (orderId, foodIds) => {
+    const query = `
       INSERT INTO order_foods (order_id, food_id)
-      VALUES ();
+      VALUES ($1, $2);
     `;
 
-  });
+    for (const foodId of foodIds) {
+      const values = [orderId, foodId];
+      db.query(query, values)
+        .catch((err) => {
+          console.log('query error:', err.message);
+        });
+    }
+  };
   return router;
 };
