3823bab8fb830beb1582d1d88b996026802f407e
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-28 17:12:11 -0500

    Load Menu Items from DB

--- server.js
+++ server.js
@@ -37,11 +37,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const menuRoutes = require("./routes/menuItems");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/menu", menuRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
