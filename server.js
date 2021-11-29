60f6ee74eec3a1f407d8543dc77b6da6522571dc
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-29 14:53:23 -0500

    added comments to menuItems.js

--- server.js
+++ server.js
@@ -37,16 +37,11 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-const menuRoutes = require("./routes/menuItems");
-const ordersRoutes = require("./routes/orders");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/api/menu", menuRoutes(db));
-app.use("/api/orders", ordersRoutes(db));
-
 // Note: mount other resources here, using the same pattern above
 
 // Home page
