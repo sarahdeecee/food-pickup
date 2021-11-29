513f2c599a378fa636b2577d88b771d8c1383b32
Miguel <mige.d@outlook.com>
2021-11-28 20:21:21 -0500

    add comment

--- routes/menuItems.js
+++ routes/menuItems.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-
+  // GET: /menu
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM food_items;`)
       .then((data) => {
         const foodItems = data.rows;
         console.log(foodItems);
-        const templateVars = { foodItems: foodItems};
+        const templateVars = { foodItems };
         res.render("menu", templateVars);
       })
       .catch((err) => {
