3373637024702def0303cb99a63a01e31ed80c97
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-30 21:02:03 -0500

    updated cart ejs page and css

--- public/scripts/updatecart.js
+++ public/scripts/updatecart.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
       $('#cart').append($item);
     }
   };
+
   // Function to ensure  text is safe
   const escape = function(str) {
     let div = document.createElement("div");
@@ -19,41 +20,27 @@ $(document).ready(function() {
 
   //dynamically create html elements
   const createCartElement = item => {
-    // console.log(item);
     const cartPage = $(`
-    <section>
-      <div>
-        <span>${item.quantity} ${item.name}</span>
-        <span class="price">$${(item.price/100).toFixed(2)}</span>
+    <section class="card">
+      <div class="items">
+        <span>
+          <span class="quantity"><i class="fas fa-plus"></i> ${item.quantity} <i class="fas fa-minus"></i></span>
+          <span class="text-left">${item.name}</span>
+        </span>
+        <span class="price text-right">$${(item.price/100).toFixed(2)}</span>
       </div>
     </section>
     `);
+    console.log(cartPage);
     return cartPage;
   };
 
   // Load tweets by getting array of tweets from /tweets
   const loadCart = () => {
-    $.ajax('/cart', { method: 'GET' })
-      .then(cartHTML => {
-        console.log(cartHTML);
-        $('#cart').empty();
-        renderCart(cartHTML);
-      })
-      .catch(err => console.error(err.message));
-    // $('#error').empty();
-    // $('#error').hide();
-    // $('.new-tweet').hide();
+    $.ajax('api/cart', { method: 'GET' })
+      .then((data) => renderCart(data));
   };
 
-  //Create new Tweet that is called within the Submit Button
-  // const addNewTweet = function(event) {
-  //   const $tweetText = $(event.target.text).serialize();
-  //   $.post('/tweets', $tweetText).then(() => {
-  //     $('#tweet-text').val(''); //clears textarea
-  //     loadTweets();
-  //   });
-  // };
-
-  // Upon document loading, load all tweets
+  // Upon document loading, load cart
   loadCart();
 });
--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,47 @@ const router  = express.Router();
 
 module.exports = (db) => {
   // GET: /cart
+  router.get("/api/cart", (req, res) => {
+    console.log('-----> get api/cart/');
+    // const values = [req.params.orderId];
+    const values = [1];
+    console.log(values);
+    const query = `
+      SELECT food_items.*, count(food_items.id) as quantity
+      FROM food_items
+      JOIN order_foods on food_id = food_items.id
+      WHERE order_foods.order_id = 1
+      GROUP BY food_items.id
+    ;`;
+    db.query(query, values)
+    .then((data) => {
+      console.log(data);
+      // check if order doesn't exist
+      if (data.rows.length === 0) {
+        return res.status(404).send();
+      }
+
+      data.rows[0].items = countItems(data.rows[0].items);
+      res.status(200).send(data.rows[0]);
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+      // .then((data) => {
+      //   console.log(data.rows);
+      //   const cart = data.rows;
+      //   const templateVars = { cart };
+      //   res.render("cart", templateVars);
+      // })
+      // .catch((err) => {
+      //   res
+      //     .status(500)
+      //     .json({ error: err.message });
+      // });
+  });
+
   router.get("/", (req, res) => {
+    console.log("-----> get /");
     const orderNumber = 1;
     db.query(`
       SELECT food_items.*, count(food_items.id) as quantity
@@ -13,7 +53,6 @@ module.exports = (db) => {
       GROUP BY food_items.id
     ;`)
       .then((data) => {
-        console.log(data.rows);
         const cart = data.rows;
         const templateVars = { cart };
         res.render("cart", templateVars);
