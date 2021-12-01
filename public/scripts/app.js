41dad4b5aa38a95712f8a180f89f1a2a029f965c
Miguel <mige.d@outlook.com>
2021-12-01 17:20:24 -0500

    fixed cart functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,49 @@
 // Client facing scripts here
-$(() => {
-
+$(document).ready(function () {
   let cartCount = 0;
   let cartItems = [];
 
-  const createItemAndAddToCart = function(value) {
-    let itemName = $.trim($(`#item-title-${value}`).text().replace(/\r?\n|\r/g, " "));
-    let itemPrice = $.trim($(`#item-price-${value}`).text().replace(/\r?\n|\r/g, " ").replace(/[^0-9]/g,''));
-    cartItems.push({"name":itemName, "price":itemPrice, "quantity": 1});
+  const createItemAndAddToCart = function (value) {
+    let itemName = $.trim(
+      $(`#item-title-${value}`)
+        .text()
+        .replace(/\r?\n|\r/g, " ")
+    );
+    let itemPrice = $.trim(
+      $(`#item-price-${value}`)
+        .text()
+        .replace(/\r?\n|\r/g, " ")
+        .replace(/[^0-9]/g, "")
+    );
+    cartItems.push({ name: itemName, price: itemPrice, quantity: 1 });
   };
 
-  $(".btn.btn-secondary.add-to-cart").click(function(event) {
+  $(".btn.btn-secondary.add-to-cart").click(function (event) {
     event.preventDefault();
     ++cartCount;
     createItemAndAddToCart($(this).val());
     $("#cartcount").text(cartCount);
   });
 
-  $("#showcart").click(function(event) {
+  $("#showcart").click(function (event) {
     event.preventDefault();
-    $.cookie('cartItems', JSON.stringify(cartItems));
+    $.cookie("cartItems", JSON.stringify(cartItems));
 
     $.ajax(`/cart`, {
       method: "POST",
-      contentType: 'application/json',
-      data: JSON.stringify({ "cartItems": cartItems }),
+      contentType: "application/json",
+      data: JSON.stringify({ cartItems }),
+      success: function () {
+        $(location).attr("href", "/cart");
+      },
     });
-
   });
 
-  $("#clearcart").click(function(event) {
+  $("#clearcart").click(function (event) {
     event.preventDefault();
     cartCount = 0;
     cartItems = [];
-    $.cookie('cartItems', JSON.stringify(cartItems));
+    $.cookie("cartItems", JSON.stringify(cartItems));
     $("#cartcount").text(cartCount);
   });
-
 });
--- routes/cart.js
+++ routes/cart.js
@@ -1,37 +1,19 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   // GET: /cart
   router.get("/", (req, res) => {
-    const orderNumber = 1;
-    db.query(`
-      SELECT food_items.*, count(food_items.id) as quantity
-      FROM food_items
-      JOIN order_foods on food_id = food_items.id
-      WHERE order_foods.order_id = ${orderNumber}
-      GROUP BY food_items.id
-    ;`)
-      .then((data) => {
-        const cart = data.rows;
-        const templateVars = { cart };
-        res.render("cart", templateVars);
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    const {cart} = req.session;
+    res.render("cart", { cart });
   });
 
-  // GET: /cart
+  // POST: /cart
   router.post("/", (req, res) => {
     const cart = req.body.cartItems;
-    const templateVars = { cart };
-    console.log(templateVars);
-    res.render("cart" ,templateVars);
+    req.session.cart = cart;
+    res.render("cart", { cart });
   });
 
   return router;
-
 };
