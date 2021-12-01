fbe4aa52694dfa78478fdea4fff13020099605e2
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-30 21:41:55 -0500

    Cart count update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,18 @@
 // Client facing scripts here
+$(() => {
+
+  /**
+   * Entered tweet is converted to HTML and returned.
+   * @param {String} str
+   * @returns HTML
+   */
+  let cartCount = 0;
+
+  $(".btn.btn-secondary.add-to-cart").click(function(event) {
+    event.preventDefault();
+    ++cartCount;
+    console.log(cartCount);
+    $("#cartcount").text(cartCount);
+  });
+
+});
