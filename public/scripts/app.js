b8fcfb497abe71bcf348d9f36bc57a96c7b11f18
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-12-01 00:03:19 -0500

    Addec clear cart changes and stored cart items in cookie

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,34 @@
 // Client facing scripts here
 $(() => {
 
-  /**
-   * Entered tweet is converted to HTML and returned.
-   * @param {String} str
-   * @returns HTML
-   */
   let cartCount = 0;
+  let cartItems = [];
+
+  const createItemAndAddToCart = function(value) {
+    let itemName = $.trim($(`#item-title-${value}`).text().replace(/\r?\n|\r/g, " "));
+    let itemPrice = $.trim($(`#item-price-${value}`).text().replace(/\r?\n|\r/g, " ").replace(/[^0-9]/g,''));
+    cartItems.push({"name":itemName, "price":itemPrice});
+  };
 
   $(".btn.btn-secondary.add-to-cart").click(function(event) {
     event.preventDefault();
     ++cartCount;
-    console.log(cartCount);
+    createItemAndAddToCart($(this).val());
+    $("#cartcount").text(cartCount);
+  });
+
+  $("#showcart").click(function(event) {
+    event.preventDefault();
+    $.cookie('cartItems', JSON.stringify(cartItems));
+    let items = $.cookie('cartItems');
+    console.log(items);
+  });
+
+  $("#clearcart").click(function(event) {
+    event.preventDefault();
+    cartCount = 0;
+    cartItems = [];
+    $.cookie('cartItems', JSON.stringify(cartItems));
     $("#cartcount").text(cartCount);
   });
 
